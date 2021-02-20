60fdfb8440c452c0c9d1dec97db950c7242871e6
BentonEdmondson <55590329+BentonEdmondson@users.noreply.github.com>
2021-02-20 17:27:39 -0600

    deleted Receiver (case sensitivity issues)

--- lib/Receiver/Receiver.cpp
+++
@@ -1,6 +0,0 @@
-#include <Receiver.h>
-
-// this class needs to be defined here
-// this class needs to give the json the correct default values
-// it also needs to give the json the correct types
-// furthermore, it will ideally prune off unnecessary properties with a filter
